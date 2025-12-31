caa58d0650f8ee6a31a5802a88315771ff64716e
Aasim Ahmed Siddiqui <93717619+SAASIMAHMEDW@users.noreply.github.com>
2025-12-31 11:42:45 +0530

    Update API base URL to use environment variable

--- frontend/src/shared/services/api.ts
+++ frontend/src/shared/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+let baseUrl = import.meta.env.VITE_BACKEND_API
 export const api = axios.create({
-    baseURL: '/api',
+    baseURL: baseUrl,
     withCredentials: true,
     timeout: 10000,
     headers: {
