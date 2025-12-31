3e29a744986f6a49d457782eed747a672815f1da
SAASIMAHMEDW <aasimahmedsiddiqui1@gmail.com>
2025-12-31 13:31:17 +0530

    fix: frontend netlify redirect

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -43,7 +43,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie('auth_token', token, {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 60 * 60 * 1000 // 1 hour
     })
     return sendSuccess(res, 'Login successful', {
